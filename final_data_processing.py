d4d1233367f624acbb45b09984d497de0277210f
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-01-17 18:06:29 +0100

    final data processed

---
+++ final_data_processing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+print("Loading...")
+df_balances = pd.read_csv("data_balances.csv", ",")
+print("Done.")
+
+print("Processing...")
+balanceList = []
+missionsList = []
+exceptionList = []
+weightList = []
+negList = []
+for i in range(0, len(df_balances)):
+    balanceList.append(df_balances.Balances[i])
+    missionsList.append(df_balances.NumMissions[i])
+    exceptionList.append(df_balances.ExceptionRate[i])
+    weightList.append(df_balances.AvgWeightDiff[i])
+    if(df_balances.NumMissions[i] != 0):
+        negList.append(df_balances.NumNegativeWeights[i] / df_balances.NumMissions[i])
+    else:
+        negList.append(0)
+print("Done.")
+
+print("Loading to csv...")
+zipped = list(zip(balanceList, missionsList, exceptionList))
+TAR_data = pd.DataFrame(zipped, columns=['Balance','Missions','TARExceptionRate'])
+zipped = list(zip(balanceList, missionsList, weightList))
+weight_data = pd.DataFrame(zipped, columns=['Balance','Missions','AverageWeightDiff'])
+zipped = list(zip(balanceList, missionsList, negList))
+neg_data = pd.DataFrame(zipped, columns=['Balance','Missions','NegativeWeightRate'])
+TAR_data.to_csv('tar_data_balances.csv')
+weight_data.to_csv('weight_data_balances.csv')
+neg_data.to_csv('neg_data_balances.csv')
+print("End.")
